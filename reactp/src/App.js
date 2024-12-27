24de111f53587a00f0df973290978026a506a52a
Lithasa <lithasa0417@gmail.com>
2024-12-27 14:27:02 +0530

    Apply some gradients for the app

--- reactp/src/App.js
+++ reactp/src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <h1>Hello World! i am lithasa</h1>
+       <h1>Hello World! I am Lithasa</h1>
     </div>
   );
 }
 
 export default App;
+
