a01f6d8feeef4cff4101324d1db5d6f024962bef
Lithasa  Jayamaha <lithasa0417@gmail.com>
2025-01-14 21:57:36 +0530

    Update with indentations

--- reactp/src/SuccessPage.js
+++ reactp/src/SuccessPage.js
@@ -5,7 +5,6 @@ const SuccessPage = () => {
         <div>
             <h1>Form Submitted Successfully!</h1>
             <p>Thank you for submitting the form.</p>
-        
         </div>
     );
 };
